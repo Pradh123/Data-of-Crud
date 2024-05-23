58dd9d644869d39f9497a975632e9452c2630630
Pradh123 <vermapradhumn3@gmail.com>
2024-05-23 18:29:42 +0530

    123456

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const jsonServer = require("json-server"); // importing json-server library
+// server.js
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json'); // db.json file should be in the root directory
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
-
 server.use(middlewares);
 server.use(router);
-
-server.listen(port);
+server.listen(process.env.PORT || 3000, () => {
+  console.log('JSON Server is running');
+});
